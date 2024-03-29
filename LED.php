857c17f051458b37223d89cb71fa6e48dba31a9c
BeNNeTTcik <42866234+BeNNeTTcik@users.noreply.github.com>
2024-03-30 00:20:52 +0100

    Update LED.php

--- LED.php
+++ LED.php
@@ -11,19 +11,19 @@
         <a href="index.php">Sensor</a>              <!-- secound webpage to see a values of sound sensor --> 
         <a class="active" href="LED.php">LED</a>    <!-- used webpage -->
   </div>
-  </div class="main">                 <!-- glowna czesc strony -->
+  </div class="main">                               
     <h1 class="title">Turn OFF Turn ON</h1>
     
-    <form action="update.php" method="post" id="LED_ON" onsubmit="myFunction()">
+    <form action="update.php" method="post" id="LED_ON" onsubmit="myFunction()">       <!-- action after click --> 
       <input type="hidden" name="stat" value="1"/>    
     </form>
     
-    <form action="update.php" method="post" id="LED_OFF">
+    <form action="update.php" method="post" id="LED_OFF">                              <!-- action after click --> 
       <input type="hidden" name="stat" value="0"/>
     </form>
     
-    <button class="buttonON" name= "subject" type="submit" form="LED_ON" value="SubmitLEDON" >LED ON</button>
-    <button class="buttonOFF" name= "subject" type="submit" form="LED_OFF" value="SubmitLEDOFF">LED OFF</button>
+    <button class="buttonON" name= "subject" type="submit" form="LED_ON" value="SubmitLEDON" >LED ON</button>        <!-- button --> 
+    <button class="buttonOFF" name= "subject" type="submit" form="LED_OFF" value="SubmitLEDOFF">LED OFF</button>     <!-- button --> 
   </div>  
 </body>
 </html>
